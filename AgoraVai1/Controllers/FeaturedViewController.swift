bed2aa1322bbc84626358b756094bb9634258875
alnp <alessandrapereira@outlook.com>
2022-07-11 17:09:48 -0300

    Adiciona outlets e começa a implementação da popular collection view

---
+++ AgoraVai1/Controllers/FeaturedViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FeaturedViewController.swift
+//  AgoraVai1
+//
+//  Created by Alessandra Pereira on 05/07/22.
+//
+
+import UIKit
+
+class FeaturedViewController: UIViewController, UICollectionViewDataSource {
+
+    @IBOutlet var popularCollectionView: UICollectionView!
+    @IBOutlet var nowPlayingCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        popularCollectionView.dataSource = self
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "popularCell", for: indexPath) as? PopularCollectionViewCell {
+            cell.titleLabel.text = "Título do Filme"
+            cell.image.image = UIImage()
+            return cell
+        }
+        
+        return UICollectionViewCell()
+    }
+
+}
+
---
+++ AgoraVai1/View/NowPlayingCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  NowPlayingCollectionViewCell.swift
+//  AgoraVai1
+//
+//  Created by Alessandra Pereira on 11/07/22.
+//
+
+import UIKit
+
+class NowPlayingCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var image: UIImageView!
+    
+}
