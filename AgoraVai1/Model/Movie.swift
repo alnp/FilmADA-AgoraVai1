0e37e090020fa3b6d427fb68004a6ab7732255f3
alnp <alessandrapereira@outlook.com>
2022-07-12 12:08:12 -0300

    Implementou as collections view e modelo

---
+++ AgoraVai1/Model/Movie.swift
@@ -0,0 +1,18 @@
+//
+//  Movie.swift
+//  AgoraVai1
+//
+//  Created by Alessandra Pereira on 12/07/22.
+//
+
+import Foundation
+
+struct Movie {
+    let title: String
+    let poster: String
+    let backdrop: String
+    let voteAverage: Float
+    let releaseDate: String
+    let overview: String
+    let genres: [String]
+}
